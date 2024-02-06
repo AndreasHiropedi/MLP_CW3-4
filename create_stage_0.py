5684487519cbe6ea1b5598d8e1bac725a52641b9
Andreas Hiropedi <s2015345@ed.ac.uk>
2024-02-06 10:07:01 +0000

    Add files via upload

---
+++ create_stage_0.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("implicit_hate_v1_stg1_posts.tsv", delimiter='\t')
+
+new_class_column = df['class'].apply(lambda x: "hate" if x == 'implicit_hate' or x == 'explicit_hate' else 'not_hate')
+
+df['class'] = new_class_column
+
+df.to_csv('implicit_hate_v1_stg0_posts.tsv', sep='\t', index=False, header=True)
